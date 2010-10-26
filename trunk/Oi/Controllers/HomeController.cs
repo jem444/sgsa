1aec74941cad5c5e6572c8068544f670c1b045af
flavinha.shi@gmail.com <flavinha.shi@gmail.com@4a421b29-8d81-67d7-a96b-cd750c53d7cf>
2010-10-26 17:51:07 +0000

    teste

---
+++ trunk/Oi/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oi.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
