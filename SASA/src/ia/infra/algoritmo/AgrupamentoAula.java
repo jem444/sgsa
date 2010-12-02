5b9faf01c0697fb4c36fba36404d723785d2458a
flavinha.shi <flavinha.shi@gmail.com>
2010-12-02 19:07:13 +0000

    Alocação de duas series em uma sala de aula, sem combinação somente 2 series determinadas.

--- SASA/src/ia/infra/algoritmo/AgrupamentoAula.java
+++ SASA/src/ia/infra/algoritmo/AgrupamentoAula.java
@@ -1,20 +1,21 @@
 package ia.infra.algoritmo;
 
-import ia.infra.negocio.horario.DiaSemana;
-
 public class AgrupamentoAula {
 	private String disciplina;
 	private int tamanho;
 	private String curso;
 	private String serie;
 	private int diaSemana;
+	private int horaAula;
+	private boolean alocado;
 	
 	public AgrupamentoAula(String disciplina, int tamanho, String curso,
-			String serie,int diasemana) {
+			String serie,int diasemana, int horaAula) {
 		this.disciplina = disciplina;
 		this.tamanho = tamanho;
 		this.curso = curso;
 		this.serie = serie;
+		this.horaAula = horaAula;
 		this.diaSemana = diasemana;
 	}
 	public AgrupamentoAula() {
@@ -54,6 +55,21 @@ public class AgrupamentoAula {
 	public int getDiaSemana() {
 		return diaSemana;
 	}
-	
+	public void setAlocado(boolean alocado) {
+		this.alocado = alocado;
+	}
+	public boolean isAlocado() {
+		return alocado;
+	}
+	public void setHoraAula(int horaAula) {
+		this.horaAula = horaAula;
+	}
+	public int getHoraAula() {
+		return horaAula;
+	}
+	@Override
+	public String toString() {
+		return this.disciplina;
+	}
 	
 }
